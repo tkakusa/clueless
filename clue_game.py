67d2efc6d460922e16802da6eadb30111e4cd3b3
TK <tkakusa@gmail.com>
2019-02-07 23:26:59 -0800

    Finished opening menu

--- clue_game.py
+++ clue_game.py
@@ -2,6 +2,15 @@ import pygame
 
 class Clueless:
     def __init__(self):
+        # Initialize the pygame module
+        pygame.init()
+
+        # Set the title of the screen
+        pygame.display.set_caption("The Game of Clue-Less")
+
+        # Create the actual screen
+        self.screen = pygame.display.set_mode((1920, 1080), pygame.RESIZABLE)
+
         # Color variables
         self.BLACK = (0, 0, 0)
         self.WHITE = (255, 255, 255)
@@ -14,6 +23,8 @@ class Clueless:
         self.MAP_HEIGHT = self.ROOM_DIMENSIONS * 5
         self.SCREEN_WIDTH = 1920
         self.SCREEN_HEIGHT = 1080
+        self.screen_opacity = 0
+        self.start_opacity = 150
 
         self.ROW_0 = 200
         self.ROW_1 = self.ROW_0 + 1 * self.ROOM_DIMENSIONS
@@ -26,26 +37,37 @@ class Clueless:
         self.COL_3 = self.COL_0 + 3 * self.ROOM_DIMENSIONS
         self.COL_4 = self.COL_0 + 4 * self.ROOM_DIMENSIONS
 
+        # Initialize clock and timer
+        self.clock = pygame.time.Clock()
+        pygame.time.set_timer(pygame.USEREVENT, 100)
+
         # Load Images
         self.logo_image = pygame.image.load('images\logo2.png')
         self.house_image = pygame.image.load('images\house.jpg')
-        self.start_image = pygame.image.load('images\start.png')
+        self.start_image = pygame.image.load('images\start.png').convert_alpha()
+        self.start_image.set_alpha(20)
 
         # Update the images
         self.update_images()
 
+        # Screen fade parameters
+        self.fade = pygame.Surface((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.fade.fill(self.BLACK)
+
         # State enums
-        self.game_state = "MENU"
-        self.menu_state = "OPENING"
+        self.program_state = "MENU"
+        self.menu_state = "INIT"
+        self.game_state = "INIT"
+        self.opacity_direction = "UP"
 
-        # Initialize the pygame module
-        pygame.init()
-
-        # Set the title of the screen
-        pygame.display.set_caption("The Game of Clue-Less")
-
-        # Create the actual screen
-        self.screen = pygame.display.set_mode((1920, 1080), pygame.RESIZABLE)
+    def blit_alpha(self, target, source, location, opacity):
+        x = location[0]
+        y = location[1]
+        temp = pygame.Surface((source.get_width(), source.get_height())).convert()
+        temp.blit(target, (-x, -y))
+        temp.blit(source, (0, 0))
+        temp.set_alpha(opacity)
+        target.blit(temp, location)
 
     def update_images(self):
         ## Transform images ##
@@ -75,18 +97,18 @@ class Clueless:
 
 
     def main_loop(self):
-
         # Variable to show if game is still running
         running = True
 
         while running:
-            if self.game_state == "MENU":
-                self.menu_loop()
-            elif self.game_state == "GAME":
-                self.game_loop()
+            # Boolean Variables
+            bool_update_opacity = False
 
             # event handler
             for event in pygame.event.get():
+                # Timer events
+                if event.type == pygame.USEREVENT:
+                    bool_update_opacity = True
                 # Resize the window in case of a resize event
                 if event.type == pygame.VIDEORESIZE:
                     self.SCREEN_WIDTH = event.w
@@ -96,7 +118,7 @@ class Clueless:
                     # Update the images
                     self.update_images()
 
-                    if self.game_state == "GAME":
+                    if self.program_state == "GAME":
                         # Draw the map
                         self.draw_map()
 
@@ -105,18 +127,91 @@ class Clueless:
                     # Exit the main loop
                     running = False
 
+            if self.program_state == "MENU":
+                self.menu_loop(bool_update_opacity)
+            elif self.program_state == "GAME":
+                self.game_loop()
+
             pygame.display.update()
 
-    def menu_loop(self):
+    def update_opacity(self, bool_update_opacity):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
 
-        # Draw the background
-        self.screen.blit(self.house_image, (0,0))
-        # Draw the logo
-        self.screen.blit(self.logo_image, [self.LOGO_X, 20])
-        # Draw the start button
-        self.screen.blit(self.start_image, [self.START_X, self.START_Y])
+        if self.START_X < mouse[0] < self.START_X + self.START_SIZE[0] and self.START_Y < mouse[1] < self.START_Y + self.START_SIZE[1]:
+            self.start_opacity = 255
+
+            # Check if the button is clicked
+            if click[0] == 1:
+                self.screen_opacity = 0
+                self.menu_state = "START"
+
+        elif bool_update_opacity:
+            if self.opacity_direction == "UP":
+                self.start_opacity = self.start_opacity + 5
+                if self.start_opacity > 200:
+                    self.opacity_direction = "DOWN"
+            else:
+                self.start_opacity = self.start_opacity - 5
+                if self.start_opacity < 100:
+                    self.opacity_direction = "UP"
+
+    def fade_screen(self, opacity):
+        fade = pygame.Surface((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        fade.fill(self.BLACK)
+        fade.set_alpha(opacity)
+        self.screen.blit(fade, (0, 0))
+        #pygame.display.update()
+
+
+    def menu_loop(self, bool_update_opacity):
+
+        # Initialize values
+        if self.menu_state == "INIT":
+            self.screen_opacity = 255
+            self.menu_state = "OPENING"
+
+        # Fade into the opening
+        if self.menu_state == "OPENING":
+            self.screen_opacity = self.screen_opacity - 2
+            self.screen.blit(self.house_image, (0, 0))
+            self.fade_screen(self.screen_opacity)
+            if self.screen_opacity < 0:
+                self.menu_state = "WAITING"
+
+        elif self.menu_state == "WAITING":
+            # Draw the background
+            self.screen.blit(self.house_image, (0,0))
+            # Draw the logo
+            self.screen.blit(self.logo_image, [self.LOGO_X, 20])
+            # Draw the start button
+            self.update_opacity(bool_update_opacity)
+            self.blit_alpha(self.screen, self.start_image, [self.START_X, self.START_Y], self.start_opacity)
+
+        elif self.menu_state == "START":
+            self.screen_opacity = self.screen_opacity + 5
+            self.screen.blit(self.house_image, (0, 0))
+            self.fade_screen(self.screen_opacity)
+            if self.screen_opacity == 255:
+                self.program_state = "GAME"
+
+
+
+    def game_loop(self):
+        if self.game_state == "INIT":
+            self.screen_opacity = 255
+            self.game_state = "FADE_IN"
+        elif self.game_state == "FADE_IN":
+            self.screen_opacity = self.screen_opacity - 2
+            self.draw_map()
+            self.fade_screen(self.screen_opacity)
+            if self.screen_opacity < 0:
+                self.game_state = "WAITING"
+
 
     def draw_map(self):
+        # Clear the screen
+        self.screen.fill(self.BLACK)
         # Room variables
         room_study = pygame.Rect(self.COL_0, self.ROW_0, self.ROOM_DIMENSIONS, self.ROOM_DIMENSIONS)
         room_hall = pygame.Rect(self.COL_0, self.ROW_2, self.ROOM_DIMENSIONS, self.ROOM_DIMENSIONS)
